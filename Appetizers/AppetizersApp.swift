c4800e1c42c636f879e7c2549bd36cd909922959
herisusantuy <herisusanto.id@gmail.com>
2023-12-28 14:27:39 +0700

    Initial Commit

---
+++ Appetizers/AppetizersApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppetizersApp.swift
+//  Appetizers
+//
+//  Created by loratech on 28/12/23.
+//
+
+import SwiftUI
+
+@main
+struct AppetizersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
